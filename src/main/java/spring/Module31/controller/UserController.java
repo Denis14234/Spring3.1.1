6c8966d66dfa0d6448d0f0a4e72da782d4bd77f6
Denis14234 <den4ik14234@gmail.com>
2025-05-12 15:50:31 +0300

    Изменения контроллера с учетом комментария

--- src/main/java/spring/Module31/controller/UserController.java
+++ src/main/java/spring/Module31/controller/UserController.java
@@ -5,58 +5,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import spring.Module31.model.User;
 import spring.Module31.service.UserService;
 
-
 @Controller
-    public class UserController {
+public class UserController {
 
-        private final UserService userService;
-        @Autowired
-        public UserController(UserService userService) {
-            this.userService = userService;
-        }
-        @GetMapping(value = "/")
-        public String redirectToUsers() {
-            return "redirect:/users";
-        }
+    private final UserService userService;
 
-        @GetMapping(value = "/users")
-        public String printUsers(@RequestParam(defaultValue = "5") int count, ModelMap model) {
-            model.addAttribute("users", userService.getUserList(count));
-            return "users";
-        }
-        @PostMapping(value = "/users/add")
-        public String addUser (@RequestParam String firstName,
-                               @RequestParam String lastName,
-                               @RequestParam String email) {
-            User user = new User(firstName, lastName, email);
-            userService.addUser (user);
-            return "redirect:/users";
-        }
-
-        @PostMapping(value = "/users/delete")
-        public String deleteUser (@RequestParam Long id) {
-            userService.deleteUser (id);
-            return "redirect:/users";
-        }
-
-        @PostMapping(value = "/users/update")
-        public String updateUser (@RequestParam Long id,
-                                  @RequestParam String firstName,
-                                  @RequestParam String lastName,
-                                  @RequestParam String email) {
-            User user = userService.getUserById(id);
-            if (user != null) {
-                user.setFirstName(firstName);
-                user.setLastName(lastName);
-                user.setEmail(email);
-                userService.updateUser (user);
-            }
-            return "redirect:/users";
-        }
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
+    @GetMapping(value = "/")
+    public String redirectToUsers() {
+        return "redirect:/users";
+    }
+
+    @GetMapping(value = "/users")
+    public String printUsers(@RequestParam(defaultValue = "5") int count, ModelMap model) {
+        model.addAttribute("users", userService.getUserList(count));
+        return "users";
+    }
+
+    @PostMapping(value = "/users/add")
+    public String addUser (@ModelAttribute User user) {
+        userService.addUser (user);
+        return "redirect:/users";
+    }
+
+    @PostMapping(value = "/users/delete")
+    public String deleteUser (@RequestParam Long id) {
+        userService.deleteUser (id);
+        return "redirect:/users";
+    }
+
+    @PostMapping(value = "/users/update")
+    public String updateUser (@ModelAttribute User user) {
+        User existingUser  = userService.getUserById(user.getId());
+        if (existingUser  != null) {
+            existingUser .setFirstName(user.getFirstName());
+            existingUser .setLastName(user.getLastName());
+            existingUser .setEmail(user.getEmail());
+            userService.updateUser (existingUser );
+        }
+        return "redirect:/users";
+    }
+}
